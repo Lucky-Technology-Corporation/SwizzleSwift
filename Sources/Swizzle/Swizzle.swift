02a58d59bfcf3caf08439ee146c3e4a5b2948d1d
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-07-31 01:09:48 -0700

    revert URL

--- Sources/Swizzle/Swizzle.swift
+++ Sources/Swizzle/Swizzle.swift
@@ -50,7 +50,6 @@ public class Swizzle {
             print("[Swizzle] ERROR - your production environment has not been set up!")
             return
         }
-        url = "http://localhost:3000"
         apiBaseURL = URL(string: url)
     }
     
