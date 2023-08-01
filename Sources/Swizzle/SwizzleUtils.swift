bc2e7aec7c783bdd35094dc5734e450094bbfd48
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-07-31 22:26:56 -0700

    Update SwizzleUtils.swift

--- Sources/Swizzle/SwizzleUtils.swift
+++ Sources/Swizzle/SwizzleUtils.swift
@@ -73,14 +73,28 @@ enum SwizzleError: LocalizedError {
 }
 #if canImport(UIKit)
 extension UIImage {
-    func resized(to size: CGSize) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(size, false, self.scale)
-        self.draw(in: CGRect(origin: .zero, size: size))
-
-        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+    func resized(to targetSize: CGSize) -> UIImage? {
+        let widthRatio  = targetSize.width  / self.size.width
+        let heightRatio = targetSize.height / self.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
-        return resizedImage
+        
+        return newImage
     }
 }
 #endif
