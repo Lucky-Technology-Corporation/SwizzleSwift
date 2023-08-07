468f87ab7d4506df66eb58a4eb5d4ca25e9ee3b8
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-08-07 15:15:12 -0700

    Update SwizzleEndpoint.swift

--- Sources/Swizzle/SwizzleEndpoint.swift
+++ Sources/Swizzle/SwizzleEndpoint.swift
@@ -41,7 +41,6 @@ public class SwizzleEndpoint<T: Codable>: ObservableObject {
     
     public var wrappedValue: T? {
         get {
-            refresh()
             return value
         }
     }
