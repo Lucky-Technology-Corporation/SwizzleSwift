58f3c451d3f5bb53dd638dc600f9061616689e2e
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-08-07 16:42:23 -0700

    MARK: start to change SwizzleStorage

--- Sources/Swizzle/SwizzleStorage.swift
+++ Sources/Swizzle/SwizzleStorage.swift
@@ -12,52 +12,57 @@ import Combine
 public class SwizzleStorage<T: Codable>: ObservableObject {
     private var observer: NSObjectProtocol?
     public let objectWillChange = PassthroughSubject<Void, Never>()
-    @Published var value: T? {
+    private let outerObjectWillChange: ObservableObjectPublisher
+    private var cancellable: AnyCancellable?
+
+    var value: T? {
         willSet {
             DispatchQueue.main.async {
                 self.objectWillChange.send()
+            }
+        }
+        didSet {
+            DispatchQueue.main.async {
+                self.outerObjectWillChange.send()
             }
         }
     }
     let key: String
     var defaultValue: T?
 
-    public init(_ key: String, defaultValue: T? = nil) {
+    public init(_ key: String, outer: ObservableObjectPublisher, defaultValue: T? = nil) {
         self.key = key
         self.defaultValue = defaultValue
+        self.outerObjectWillChange = outer
 
         if let data = Swizzle.shared.userDefaults.data(forKey: key), let loadedValue = try? JSONDecoder().decode(T.self, from: data) {
             self.value = loadedValue
             self.objectWillChange.send()
             refresh()
         }
-        
-        observer = NotificationCenter.default.addObserver(forName: .swizzleModelUpdated, object: nil, queue: nil) { [weak self] _ in
-            self?.refreshFromCache()
+
+        self.cancellable = self.objectWillChange.sink { [weak self] in
+            DispatchQueue.main.async {
+                self?.outerObjectWillChange.send()
+            }
         }
     }
     
     deinit {
-        if let observer = observer {
-            NotificationCenter.default.removeObserver(observer)
-        }
+        cancellable?.cancel()
     }
     
     public var wrappedValue: T? {
         get { value }
         set {
-            DispatchQueue.main.async { [weak self] in
-                self?.value = newValue
-            }
+            value = newValue
             if let newValue = newValue {
                 Swizzle.shared.saveValue(newValue, forKey: key)
-                NotificationCenter.default.post(name: .swizzleStorageUpdated, object: nil)
             } else {
                 print("[Swizzle] Can't update a property of a nil object.")
             }
         }
     }
-
     
     public var projectedValue: SwizzleStorage { self }
     
@@ -65,7 +70,6 @@ public class SwizzleStorage<T: Codable>: ObservableObject {
         Swizzle.shared.loadValue(forKey: key, defaultValue: defaultValue) { [weak self] fetchedValue in
             DispatchQueue.main.async {
                 self?.value = fetchedValue
-                self?.objectWillChange.send()
                 do {
                     let data = try JSONEncoder().encode(fetchedValue)
                     Swizzle.shared.userDefaults.set(data, forKey: self?.key ?? "")
@@ -73,42 +77,5 @@ public class SwizzleStorage<T: Codable>: ObservableObject {
             }
         }
     }
-    
-    public func refreshFromCache(){
-        if let data = Swizzle.shared.userDefaults.data(forKey: key), let loadedValue = try? JSONDecoder().decode(T.self, from: data) {
-            self.value = loadedValue
-            self.objectWillChange.send()
-        }
-    }
-    
-}
 
-
-public class SwizzleModel<T: Codable>: ObservableObject {
-    @SwizzleStorage("") public var object: T? {
-        didSet{
-            NotificationCenter.default.post(name: .swizzleModelUpdated, object: nil)
-        }
-    }
-
-    private var cancellables = Set<AnyCancellable>()
-
-    public init(_ key: String, defaultValue: T? = nil) {
-        _object = SwizzleStorage(key, defaultValue: defaultValue)
-        _object.objectWillChange
-            .sink { [weak self] _ in
-                self?.objectWillChange.send()
-            }
-            .store(in: &cancellables)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(updateValue), name: .swizzleStorageUpdated, object: nil)
-    }
-    
-    @objc func updateValue(){
-        _object.refreshFromCache()
-    }
-    
-    public func refresh(){
-        _object.refresh()
-    }
 }
